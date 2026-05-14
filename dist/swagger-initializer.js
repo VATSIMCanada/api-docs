5b0935a2e006340116d64421fb56d4d27e611c09
codeman02 <codeman02@gmail.com>
2026-05-14 08:38:13 -0400

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://https://api.vatcan.ca/swagger.yaml",
+    url: "https://api.vatcan.ca/swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
